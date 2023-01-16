7861f427451951d3a05ee5632f5834669a8fb0e8
Delekson <manikavasakard@gmail.com>
2023-01-16 16:13:48 +0000

    New snake direction mechanics and blinking objects bug fix

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import pygame
 from pygame import time
 import random
+import math
 
-size = 30
+size = 25
 width = 800
-height = 600
+height = 800
 bg = pygame.image.load('resources/background.jpg')
 
 class Apple:
     def __init__(self):
-        self.app_block = pygame.image.load('resources/apple_1.jpg')
+        self.app_block = pygame.image.load('resources/apple.jpg')
         self.app_x = size
         self.app_y = size
 
     def draw_app(self, screen):
         screen.blit(self.app_block, (self.app_x, self.app_y))
-        pygame.display.flip()
 
     def move_app(self, screen):
         self.app_x = random.randint(0,(round(width/size)-1))*size
@@ -24,17 +24,24 @@ class Apple:
 class Snake:
     def __init__(self, length): 
         self.length = length
+        self.head = pygame.image.load("resources/snake_head.jpg")
         self.block = pygame.image.load("resources/block.jpg")
         self.x = [size]*length
         self.y = [size]*length
-        self.direction = 'down'
+        self.direction_lu = {
+            "up": (0,1),
+            "down": (0,-1),
+            "left": (-1,0),
+            "right": (1,0)}
+        self.direction = self.direction_lu["down"]
 
     def draw(self, screen):
-        #screen.fill((0,0,0))
         screen.blit(bg, (0,0))
         for i in range(self.length):
-            screen.blit(self.block, (self.x[i], self.y[i]))
-        pygame.display.flip()
+            if i == 0:
+                screen.blit(self.head, (self.x[i], self.y[i]))
+            else:
+                screen.blit(self.block, (self.x[i], self.y[i]))
 
     def increase_length(self, screen):
         self.length += 1
@@ -42,32 +49,38 @@ class Snake:
         self.y.append(0)
 
     def move_up(self, screen):
-        self.direction = 'up'
+        self.direction = self.direction_lu["up"]
 
     def move_down(self, screen):
-        self.direction = 'down'
+        self.direction = self.direction_lu["down"]
 
     def move_left(self, screen):
-        self.direction = 'left'
+        self.direction = self.direction_lu["left"]
 
     def move_right(self, screen):
-        self.direction = 'right'
+        self.direction = self.direction_lu["right"]
+    
+    def rotation(self, new_direction):
+        self.dot = (self.direction[0] * self.direction_lu[new_direction][0]) + (self.direction[1] * self.direction_lu[new_direction][1])
+        self.det = (self.direction[0] * self.direction_lu[new_direction][1]) - (self.direction[1] * self.direction_lu[new_direction][0])
+
+        return math.degrees(math.atan2(self.det, self.dot))
 
     def walk(self, screen):
         for i in range(self.length-1, 0, -1):
             self.x[i] = self.x[i-1]
             self.y[i] = self.y[i-1]
 
-        if self.direction == 'up':
+        if self.direction == self.direction_lu["up"]: 
             self.y[0] += -size
             self.draw(screen)
-        elif self.direction == 'down':
+        elif self.direction == self.direction_lu["down"]: 
             self.y[0] += size
             self.draw(screen)
-        elif self.direction == 'left':
+        elif self.direction == self.direction_lu["left"]: 
             self.x[0] += -size
             self.draw(screen)
-        elif self.direction == 'right':
+        elif self.direction == self.direction_lu["right"]:
             self.x[0] += size
             self.draw(screen)
 
@@ -78,7 +91,6 @@ class Game:
         pygame.mixer.init()
         #Making it a class member so it can be access later on in the program/other class functions
         self.screen = pygame.display.set_mode((width, height))
-        #self.screen.fill((0, 0, 0))
         self.snake = Snake(1)
         self.snake.draw(self.screen)
         self.apple = Apple()
@@ -98,10 +110,8 @@ class Game:
 
     def render_background(self):
         self.screen.blit(bg, (0, 0))
-        pygame.display.flip()
 
     def play(self):
-        self.render_background()
         self.snake.walk(self.screen)
         self.apple.draw_app(self.screen)
         self.display_score()
@@ -109,17 +119,9 @@ class Game:
         if self.is_collision(self.apple.app_x, self.snake.x[0], self.apple.app_y, self.snake.y[0]):
             self.apple.move_app(self.screen)
             self.snake.increase_length(self.screen)
-
+        
         if self.is_snake_col(self.snake.x[0], self.snake.y[0], self.snake.x[3:], self.snake.y[3:]):
-            # print([self.snake.x[0], self.snake.y[0], self.snake.x[3:], self.snake.y[3:]])
             raise 'Game over'
-
-        # for i in range(2, self.snake.length):
-        #     if self.is_collision(self.snake.x[0], self.snake.x[i], self.snake.y[0], self.snake.y[i]):
-        #         print("You lose!")
-        #         exit(0)
-        #This method doesn't work as it checks the whole list of snake positions and has no sense of coordinates
-        pygame.display.flip()
 
         pygame.display.flip()
         time.wait(100)
@@ -153,15 +155,32 @@ class Game:
                 
                 if event.type == pygame.KEYDOWN:
                     if (event.key == pygame.K_UP)&(pause is False):
+                        if self.snake.direction != (0,1):
+                            self.cw_rotation = self.snake.rotation("up")
+                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
+
                         self.snake.move_up(self.screen)
 
                     if (event.key == pygame.K_DOWN)&(pause is False):
+                        if self.snake.direction != (0,-1):
+                            self.cw_rotation = self.snake.rotation("down")
+                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
+
                         self.snake.move_down(self.screen)
+                        
 
                     if (event.key == pygame.K_LEFT)&(pause is False):
+                        if self.snake.direction != (-1,0):
+                            self.cw_rotation = self.snake.rotation("left")
+                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
+
                         self.snake.move_left(self.screen)
 
                     if (event.key == pygame.K_RIGHT)&(pause is False):
+                        if self.snake.direction != (1,0):
+                            self.cw_rotation = self.snake.rotation("right")
+                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
+
                         self.snake.move_right(self.screen)
                     
                     if (event.key == pygame.K_RETURN)&(pause is True):
