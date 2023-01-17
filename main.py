d84b77884df9892bc58a066daec51c43356e2ee2
Delekson <manikavasakard@gmail.com>
2023-01-17 10:06:04 +0000

    Changed apple/snake collision to look cleaner, changed snake movement mechanic to use deque

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-from pygame import time
-import random
-import math
+from random import randint
+from math import atan2,degrees
+from collections import deque
 
 size = 25
 width = 800
@@ -10,30 +10,45 @@ bg = pygame.image.load('resources/background.jpg')
 
 class Apple:
     def __init__(self):
+        """
+        Initialising the apple object at a pseudo-random location
+        """
         self.app_block = pygame.image.load('resources/apple.jpg')
-        self.app_x = size
-        self.app_y = size
+
+        self.app_x = randint(0,(round(width/size)-1))*size
+        self.app_y = randint(0,(round(height/size)-1))*size
 
     def draw_app(self, screen):
+        """
+        Draws the apple at coordinates app_x and app_y
+        """
         screen.blit(self.app_block, (self.app_x, self.app_y))
 
     def move_app(self, screen):
-        self.app_x = random.randint(0,(round(width/size)-1))*size
-        self.app_y = random.randint(0,(round(height/size)-1))*size
+        """
+        Creates a set of pseudo-random  coordinates for the next apple
+        """
+        self.app_x = randint(0,(round(width/size)-1))*size
+        self.app_y = randint(0,(round(height/size)-1))*size
 
 class Snake:
     def __init__(self, length): 
-        self.length = length
+        """
+        Initialising a snake object
+        """
         self.head = pygame.image.load("resources/snake_head.jpg")
         self.block = pygame.image.load("resources/block.jpg")
-        self.x = [size]*length
-        self.y = [size]*length
         self.direction_lu = {
-            "up": (0,1),
-            "down": (0,-1),
-            "left": (-1,0),
-            "right": (1,0)}
+                            "up": (0,1),
+                            "down": (0,-1),
+                            "left": (-1,0),
+                            "right": (1,0)
+                            }
+
+        self.length = length
         self.direction = self.direction_lu["down"]
+        self.x = deque([size])
+        self.y = deque([size])
 
     def draw(self, screen):
         screen.blit(bg, (0,0))
@@ -42,47 +57,38 @@ class Snake:
                 screen.blit(self.head, (self.x[i], self.y[i]))
             else:
                 screen.blit(self.block, (self.x[i], self.y[i]))
-
+        
     def increase_length(self, screen):
         self.length += 1
         self.x.append(0)
         self.y.append(0)
 
-    def move_up(self, screen):
-        self.direction = self.direction_lu["up"]
-
-    def move_down(self, screen):
-        self.direction = self.direction_lu["down"]
-
-    def move_left(self, screen):
-        self.direction = self.direction_lu["left"]
-
-    def move_right(self, screen):
-        self.direction = self.direction_lu["right"]
+    def change_direction(self, direction, screen):
+        self.direction = self.direction_lu[direction]
     
     def rotation(self, new_direction):
         self.dot = (self.direction[0] * self.direction_lu[new_direction][0]) + (self.direction[1] * self.direction_lu[new_direction][1])
         self.det = (self.direction[0] * self.direction_lu[new_direction][1]) - (self.direction[1] * self.direction_lu[new_direction][0])
 
-        return math.degrees(math.atan2(self.det, self.dot))
+        return degrees(atan2(self.det, self.dot))
 
     def walk(self, screen):
-        for i in range(self.length-1, 0, -1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
-
         if self.direction == self.direction_lu["up"]: 
-            self.y[0] += -size
-            self.draw(screen)
+            self.y.appendleft(self.y[0] + (-size))
+            self.x.appendleft(self.x[0])
         elif self.direction == self.direction_lu["down"]: 
-            self.y[0] += size
-            self.draw(screen)
-        elif self.direction == self.direction_lu["left"]: 
-            self.x[0] += -size
-            self.draw(screen)
-        elif self.direction == self.direction_lu["right"]:
-            self.x[0] += size
-            self.draw(screen)
+            self.y.appendleft(self.y[0] + (size))
+            self.x.appendleft(self.x[0])
+        elif self.direction == self.direction_lu["left"]:
+            self.y.appendleft(self.y[0]) 
+            self.x.appendleft(self.x[0] + (-size))
+        else:
+            self.y.appendleft(self.y[0]) 
+            self.x.appendleft(self.x[0] + (size))
+
+        self.y.pop()
+        self.x.pop()
+        self.draw(screen)
 
 class Game:
     def __init__(self):
@@ -91,44 +97,60 @@ class Game:
         pygame.mixer.init()
         #Making it a class member so it can be access later on in the program/other class functions
         self.screen = pygame.display.set_mode((width, height))
+
         self.snake = Snake(1)
         self.snake.draw(self.screen)
+
         self.apple = Apple()
         self.apple.draw_app(self.screen)
 
-    def is_collision(self, x1, x2, y1, y2):
-        if  y1 == y2:
-            if x1 == x2:
-                return True
-        return False
+    def is_collision(self, apple_x, snake_x, apple_y, snake_y):
+        """
+        Check if there is a collision between an apple and the snake's head
+        """
+        test_1 = self.snake.direction[0] - self.snake.direction[1]
+
+        if test_1 < 0:
+            apple_x -= 25*self.snake.direction[0]
+            apple_y += 25*self.snake.direction[1]
+        else:
+            snake_x += 25*self.snake.direction[0]
+            snake_y -= 25*self.snake.direction[1]
+
+        return (apple_y == snake_y) & (apple_x == snake_x)
  
-    def is_snake_col(self, x1, y1, other_x, other_y):
+    def is_snake_col(self, x1, y1, x_body, y_body):
+        other_x = x_body.copy()
+        other_y = y_body.copy()
+
+        other_x.popleft()
+        other_y.popleft()
+
         for i in range(len(other_x)):
-            if x1 == other_x[i] and y1 == other_y[i]:
-                return True
-        return False
+            return self.is_collision(other_x[i], x1, other_y[i], y1)
+            #return (x1 == other_x[i] & y1 == other_y[i])
 
     def render_background(self):
         self.screen.blit(bg, (0, 0))
 
-    def play(self):
+    def play(self): 
+        if self.is_collision(self.apple.app_x, self.snake.x[0], self.apple.app_y, self.snake.y[0]):
+            self.apple.move_app(self.screen)
+            self.apple.draw_app(self.screen)
+            self.snake.increase_length(self.screen)
+
+        if self.is_snake_col(self.snake.x[0], self.snake.y[0], self.snake.x, self.snake.y):
+            raise 'Game over'
+
         self.snake.walk(self.screen)
         self.apple.draw_app(self.screen)
         self.display_score()
-
-        if self.is_collision(self.apple.app_x, self.snake.x[0], self.apple.app_y, self.snake.y[0]):
-            self.apple.move_app(self.screen)
-            self.snake.increase_length(self.screen)
-        
-        if self.is_snake_col(self.snake.x[0], self.snake.y[0], self.snake.x[3:], self.snake.y[3:]):
-            raise 'Game over'
-
         pygame.display.flip()
-        time.wait(100)
+        pygame.time.wait(120)
 
     def display_score(self):
-        font = pygame.font.SysFont('arial', 30)
-        score = font.render('Score: ' + str(self.snake.length), True, (255, 255, 255))
+        font = pygame.font.SysFont('Courier New', 30, bold = pygame.font.Font.bold)
+        score = font.render('Score: ' + str(self.snake.length-1), True, (255, 255, 255),(0,153,0))
         self.screen.blit(score, (0, 0))
 
     def game_over(self):
@@ -154,42 +176,21 @@ class Game:
                     running = False
                 
                 if event.type == pygame.KEYDOWN:
-                    if (event.key == pygame.K_UP)&(pause is False):
-                        if self.snake.direction != (0,1):
-                            self.cw_rotation = self.snake.rotation("up")
+                    key = pygame.key.name(event.key)
+                    if (key in self.snake.direction_lu)&(pause is False):
+                        if self.snake.direction != self.snake.direction_lu[key]:
+                            self.cw_rotation = self.snake.rotation(key)
                             self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
 
-                        self.snake.move_up(self.screen)
+                        self.snake.change_direction(key, self.screen)
 
-                    if (event.key == pygame.K_DOWN)&(pause is False):
-                        if self.snake.direction != (0,-1):
-                            self.cw_rotation = self.snake.rotation("down")
-                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
-
-                        self.snake.move_down(self.screen)
-                        
-
-                    if (event.key == pygame.K_LEFT)&(pause is False):
-                        if self.snake.direction != (-1,0):
-                            self.cw_rotation = self.snake.rotation("left")
-                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
-
-                        self.snake.move_left(self.screen)
-
-                    if (event.key == pygame.K_RIGHT)&(pause is False):
-                        if self.snake.direction != (1,0):
-                            self.cw_rotation = self.snake.rotation("right")
-                            self.snake.head = pygame.transform.rotate(self.snake.head, self.cw_rotation)
-
-                        self.snake.move_right(self.screen)
-                    
                     if (event.key == pygame.K_RETURN)&(pause is True):
                         pause = False
 
             try:
                 if not pause:
                     self.play()
-            except Exception as e:
+            except Exception:
                 self.game_over()
                 pause = True
                 self.reset()
